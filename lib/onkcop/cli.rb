53ee01609640bb60eaac661512f60502dd004dc4
Takafumi ONAKA <takafumi.onaka@gmail.com>
2016-12-28 09:45:44 +0900

    Get ready to support command-line options

--- lib/onkcop/cli.rb
+++ lib/onkcop/cli.rb
@@ -2,7 +2,7 @@ require "fileutils"
 module Onkcop
   class CLI
     def self.start(args)
-      action_name = args.first
+      action_name = retrieve_command_name(args)
       unless action_name
         puts "onkcop commands:"
         puts "  init - Setup .rubocop.yml"
@@ -11,16 +11,25 @@ module Onkcop
 
       instance = self.new
       if instance.public_methods(false).include?(action_name.to_sym)
-        instance.__send__(action_name)
+        instance.__send__(action_name, args)
         exit
       end
 
       puts "Could not find command #{action_name}."
       exit(1)
+    rescue => e
+      puts e.message
+      exit(1)
+    end
+
+    def self.retrieve_command_name(args)
+      meth = args.first.to_s unless args.empty?
+      args.shift if meth && (meth !~ /^\-/)
     end
 
     CONFIG_FILE_NAME = ".rubocop.yml"
-    def init
+    def init(args)
+      raise "usage: onkcop init" unless args.empty?
       template_path = File.expand_path("../../templates", __dir__)
       puts "#{File.exist?(CONFIG_FILE_NAME) ? "overwrite" : "create"} #{CONFIG_FILE_NAME}"
       FileUtils.copy_file(File.join(template_path, CONFIG_FILE_NAME), CONFIG_FILE_NAME)
